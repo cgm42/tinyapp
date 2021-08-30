04a32f449de76c5ee1d5377320ad8ac59e12b8b3
Carrie <carr1eg@hotmail.com>
2021-08-30 14:45:12 -0700

    added single url display page

--- express_server.js
+++ express_server.js
@@ -5,19 +5,27 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
-  const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-  };
   res.render('urls_index', {
     urlDatabase,
   });
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+   const templateVars = {
+    urlShort: req.params.shortURL,
+    urlLong: urlDatabase[req.params.shortURL]
+   }
+  res.render('urls_show', templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
