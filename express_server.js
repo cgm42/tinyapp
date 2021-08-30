62233beb9fb7d48c72a127d684b5cf937b52761b
Carrie G <carr1eg@Hotmail.com>
2021-08-30 21:27:54 +0000

    set up table for url

--- express_server.js
+++ express_server.js
@@ -5,15 +5,20 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const urlDatabase = {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+  };
+  res.render('urls_index', {
+    urlDatabase,
+  });
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
