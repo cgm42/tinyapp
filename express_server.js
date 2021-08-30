c7a96297f907cd77018bad3be3b37b7112b29810
Carrie G <carr1eg@Hotmail.com>
2021-08-30 23:40:36 +0000

    added redirect for new url post

--- express_server.js
+++ express_server.js
@@ -40,7 +40,9 @@ app.get("/urls.json", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const urlShort = generateRandomString();
+  urlDatabase[urlShort] = req.body.urlLong;
+  res.redirect(`/urls/${urlShort}`);
 });
 
 app.listen(PORT, () => {
@@ -50,9 +52,14 @@ app.listen(PORT, () => {
 const generateRandomString = () => {
   const result = [];
   while (result.length < 6) {
-    const letter = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    if(Math.random() > 0.5) letter = letter.toLowerCase();
-    result.push(letter);
+    let rand;
+    if(Math.random() < 10 / (10 + 26 * 2)) { //assign number probability in addition to alphabet
+      rand = Math.floor(Math.random() * 10);
+    } else {
+      rand = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+      if(Math.random() > 0.5) rand = rand.toLowerCase();
+    }
+    result.push(rand);
   }
   return result.join('');
 }
